097586ec4d22918e536a16448b818db745669a28
Svetlana <bulatova44@gmail.com>
2022-08-05 20:39:04 +0300

    Added the Positive Numbers() function that generates three-digit positive numbers.

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int PositiveNumbers()
+{
+    int posNum = new Random().Next(100, 1000);
+    return posNum;
+}
+
+System.Console.WriteLine(PositiveNumbers());
